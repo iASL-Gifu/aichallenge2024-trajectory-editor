6631d123115614fde8accda0a1e43fd303c910c9
knorrrr <99851410+knorrrr@users.noreply.github.com>
2024-09-27 20:40:59 +0900

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # ros2 run my_package my_node --ros-args -p my_param:=my_value
-ros2 run csv_editor csv_editor_node --ros-args -p base_path:=$1
+source /opt/ros/humble/setup.bash >/dev/null
+source /aichallenge/workspace/install/setup.bash >/dev/null
+ros2 run csv_editor csv_editor_node --ros-args -p base_path:=$1 >/dev/null
