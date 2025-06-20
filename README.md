cf636baad5350678199eb7d2528240731d95eb41
knorrrr <kunotomoki61@gmail.com>
2025-06-20 17:12:08 +0900

    Revert "Revert "Revert "apply ci"""
    This reverts commit 3f857b3edd26c337aee03484ed8e8958558cb8f0.

--- README.md
+++ README.md
@@ -1,23 +1,19 @@
 # aichallenge-trajectory-editor
-
 Provides a trajectory editor and a tool for generating trajectory from raceline
 
 🍀We have implemented it in Rviz. The usage is described in this [link](./rviz_plugins.md).
 
 ## how to run
-
 ```bash
 export PATH="$PATH:$HOME/aichallenge2024-trajectory-editor/cmd_line/"
 ```
 
 run editor
-
 ```bash
 csv_editor
 ```
 
 run enerating trajectory from raceline
-
 ```bash
 raceline_to_traj
 ```
--- rviz_editor_plugins/include/rviz_editor_plugins/csv_marker_display.hpp
+++ rviz_editor_plugins/include/rviz_editor_plugins/csv_marker_display.hpp
@@ -1,23 +1,25 @@
-#ifndef RVIZ_EDITOR_PLUGINS__CSV_MARKER_DISPLAY_HPP_
-#define RVIZ_EDITOR_PLUGINS__CSV_MARKER_DISPLAY_HPP_
+# ifndef RVIZ_EDITOR_PLUGINS_CSV_MARKER_DISPLAY_HPP
+# define RVIZ_EDITOR_PLUGINS_CSV_MARKER_DISPLAY_HPP
 
-#ifndef Q_MOC_RUN
-#include "editor_tool_srvs/srv/load_csv.hpp"
-#include "editor_tool_srvs/srv/select_range.hpp"
-#include "visualization_msgs/msg/marker_array.hpp"
-#include <QtWidgets>
-#include <rclcpp/rclcpp.hpp>
-#include <rviz_common/panel.hpp>
-#endif
+# ifndef Q_MOC_RUN
+# include <rclcpp/rclcpp.hpp>
+# include <rviz_common/panel.hpp>
+# include <QtWidgets>
+# include "visualization_msgs/msg/marker_array.hpp"
+# include "editor_tool_srvs/srv/load_csv.hpp"
+# include "editor_tool_srvs/srv/select_range.hpp"
+# endif
 
-namespace rviz_editor_plugins {
-class CsvMarkerDisplay : public rviz_common::Panel {
+namespace rviz_editor_plugins
+{
+class CsvMarkerDisplay : public rviz_common::Panel
+{
   Q_OBJECT
 public:
-  CsvMarkerDisplay(QWidget *parent = nullptr);
+  CsvMarkerDisplay(QWidget * parent = nullptr);
 
   virtual void onInitialize();
-  virtual void load(const rviz_common::Config &config);
+  virtual void load(const rviz_common::Config & config);
   virtual void save(rviz_common::Config config) const;
 
 public Q_SLOTS:
@@ -25,14 +27,15 @@ public Q_SLOTS:
 
 protected:
   rclcpp::Node::SharedPtr node_;
-  QLabel *label_;
-  QPushButton *load_button_;
-  QPushButton *save_button_;
+  QLabel* label_;
+  QPushButton * load_button_;
+  QPushButton * save_button_;
   rclcpp::Client<editor_tool_srvs::srv::LoadCsv>::SharedPtr load_client_;
   void loadCsv();
   void saveCsv();
 };
 
-} // namespace rviz_editor_plugins
+}
 
-#endif // RVIZ_EDITOR_PLUGINS__CSV_MARKER_DISPLAY_HPP_
+# endif // RVIZ_EDITOR_PLUGINS_CSV_MARKER_DISPLAY_HPP
+  
